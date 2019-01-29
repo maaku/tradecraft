a7958ef65103767270accd81b6b9e4f48c315d9c
Mark Friedenbach <mark@friedenbach.org>
2019-01-29 06:58:05 -0800

    [Version] Switch to Freicoin/Tradecraft versioning scheme for version strings.
    In comparison to bitcoin release numbers, we promote minor version numbers to major, so for example release "0.11.3" becomes "11.3"; we repurpose the revision number as a subproject release number, so "11.3.1" is the second release based of upstream "0.11.3"; we add a "patch" level to the version scheme, a 4th field which indicates Freicoin/Tradecraft updates within the same upstream release; and we repurpose the build number as a monotonically increasing release number.

--- src/clientversion.h
+++ src/clientversion.h
@@ -25,9 +25,10 @@
  */
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR 0
-#define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 2
+#define CLIENT_VERSION_MAJOR 14
+#define CLIENT_VERSION_MINOR 3
+#define CLIENT_VERSION_REVISION 0
+#define CLIENT_VERSION_PATCH 0
 #define CLIENT_VERSION_BUILD 0
 
 //! Set to true for release, false for prerelease or test build
@@ -66,7 +67,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
-                         +       1 * CLIENT_VERSION_BUILD;
+                         +           CLIENT_VERSION_PATCH;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
