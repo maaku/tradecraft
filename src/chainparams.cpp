d272465305f86881df27201f7e47d38d6f708775
Mark Friedenbach <mark@friedenbach.org>
2019-02-26 16:37:38 +0100

    [Soft-fork] Require the nTimeLock value of a coinbase transaction to be equal to the median of the past 11 block times.
    This is an old-style supermajority rollout with BIP8/BIP9-like version bits and BIP8-like activation-on-timeout semantics.  It's not strictly compatible with those BIPs though, as we didn't want to back port all the necessary state management code.  But miners using BIP8/BIP9 compatible version bit software were able to signal for this rollout by configuring their devices to signal bit #28 (the highest-order BIP8/BIP9 bit).  Bit 28 needs to be set during the activation period between April 2, 2019 and October 2, 2019 for a block to be considered valid.  This is residual condition from a pre-versionbits soft-fork deployment mechanism, and should not trigger the normal warnings about unexpected versionbits deployments.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -90,7 +90,7 @@ public:
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
         consensus.nMinerConfirmationWindow = 2016; // OriginalTargetTimespan() / nPowTargetSpacing
-        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 27;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
 
@@ -106,6 +106,12 @@ public:
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000001bfc1acb352e9310223");
+
+        // Locked-in via checkpoint:
+        consensus.verify_coinbase_lock_time_activation_height = 247554;
+        // Wednesday, October 2, 2019 00:00:00 UTC
+        // This is 4PM PDT, 7PM EDT, and 9AM JST.
+        consensus.verify_coinbase_lock_time_timeout = 1569974400;
 
         consensus.original_adjust_interval = 2016; // two weeks
         consensus.filtered_adjust_interval = 9; // 1.5 hrs
@@ -214,7 +220,7 @@ public:
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1512; // 75% for testchains
         consensus.nMinerConfirmationWindow = 2016; // OriginalTargetTimespan() / nPowTargetSpacing
-        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 27;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
 
@@ -230,6 +236,10 @@ public:
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000100010001");
+
+        consensus.verify_coinbase_lock_time_activation_height = 2016;
+        // Tuesday, April 2, 2019 00:00:00 UTC
+        consensus.verify_coinbase_lock_time_timeout = 1554163200;
 
         consensus.original_adjust_interval = 2016; // two weeks
         consensus.filtered_adjust_interval = 9; // 1.5 hrs
@@ -305,7 +315,7 @@ public:
         consensus.fPowNoRetargeting = true;
         consensus.nRuleChangeActivationThreshold = 108; // 75% for testchains
         consensus.nMinerConfirmationWindow = 144; // Faster than normal for regtest (144 instead of 2016)
-        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+        consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 27;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 0;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 999999999999ULL;
         consensus.vDeployments[Consensus::DEPLOYMENT_LOCKTIME].bit = 0;
@@ -317,6 +327,9 @@ public:
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x00");
+
+        consensus.verify_coinbase_lock_time_activation_height = std::numeric_limits<int64_t>::max();
+        consensus.verify_coinbase_lock_time_timeout = 1356123600;
 
         consensus.original_adjust_interval = 2016; // two weeks
         consensus.filtered_adjust_interval = 9; // 1.5 hrs
