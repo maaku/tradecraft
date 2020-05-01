6ab7fa9a699ddb54268c02b57c21efd717ca6fdf
Mark Friedenbach <mark@friedenbach.org>
2020-04-30 18:49:16 -0700

    [Stratum] Add initial stratum server configuration code.
    Sets up a network listener on the Stratum port, and log connections.

--- src/init.cpp
+++ src/init.cpp
@@ -42,6 +42,7 @@
 #include "script/standard.h"
 #include "script/sigcache.h"
 #include "scheduler.h"
+#include "stratum.h"
 #include "timedata.h"
 #include "txdb.h"
 #include "txmempool.h"
@@ -80,6 +81,7 @@ using namespace std;
 bool fFeeEstimatesInitialized = false;
 static const bool DEFAULT_PROXYRANDOMIZE = true;
 static const bool DEFAULT_REST_ENABLE = false;
+static const bool DEFAULT_STRATUM_ENABLE = false;
 static const bool DEFAULT_DISABLE_SAFEMODE = false;
 static const bool DEFAULT_STOPAFTERBLOCKIMPORT = false;
 
@@ -179,6 +181,7 @@ static boost::scoped_ptr<ECCVerifyHandle> globalVerifyHandle;
 
 void Interrupt(boost::thread_group& threadGroup)
 {
+    InterruptStratumServer();
     InterruptHTTPServer();
     InterruptHTTPRPC();
     InterruptRPC();
@@ -205,6 +208,7 @@ void Shutdown()
     StopHTTPRPC();
     StopREST();
     StopRPC();
+    StopStratumServer();
     StopHTTPServer();
 #ifdef ENABLE_WALLET
     if (pwalletMain)
@@ -489,7 +493,10 @@ std::string HelpMessage(HelpMessageMode mode)
     }
 
     strUsage += HelpMessageGroup(_("Stratum server options:"));
+    strUsage += HelpMessageOpt("-stratum", _("Enable stratum server (default: off)"));
+    strUsage += HelpMessageOpt("-stratumbind=<addr>", _("Bind to given address to listen for Stratum work requests. Use [host]:port notation for IPv6. This option can be specified multiple times (default: bind to all interfaces)"));
     strUsage += HelpMessageOpt("-stratumport=<port>", strprintf(_("Listen for Stratum work requests on <port> (default: %u or testnet: %u)"), BaseParams(CBaseChainParams::MAIN).StratumPort(), BaseParams(CBaseChainParams::TESTNET).StratumPort()));
+    strUsage += HelpMessageOpt("-stratumallowip=<ip>", _("Allow Stratum work requests from specified source. Valid for <ip> are a single IP (e.g. 1.2.3.4), a network/netmask (e.g. 1.2.3.4/255.255.255.0) or a network/CIDR (e.g. 1.2.3.4/24). This option can be specified multiple times"));
 
     return strUsage;
 }
@@ -684,6 +691,8 @@ bool AppInitServers(boost::thread_group& threadGroup)
     RPCServer::OnStopped(&OnRPCStopped);
     RPCServer::OnPreCommand(&OnRPCPreCommand);
     if (!InitHTTPServer())
+        return false;
+    if (GetBoolArg("-stratum", DEFAULT_STRATUM_ENABLE) && !InitStratumServer())
         return false;
     if (!StartRPC())
         return false;
---
+++ src/stratum.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 The Freicoin Developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#ifndef FREICOIN_STRATUM_H
+#define FREICOIN_STRATUM_H
+
+#include "netbase.h"
+
+#include <event2/event.h>
+
+/** Configure the stratum server. */
+bool InitStratumServer();
+
+/** Interrupt the stratum server connections. */
+void InterruptStratumServer();
+
+/** Cleanup stratum server network connections and free resources. */
+void StopStratumServer();
+
+#endif // FREICOIN_STRATUM_H
+
+// End of File
