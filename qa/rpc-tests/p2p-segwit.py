aaeebb9a58b0c58e853575221c679b69dafeb525
Mark Friedenbach <mark@friedenbach.org>
2020-06-15 14:42:44 -0700

    [MergeMining] Add versionbits deployment information for merge mining.

--- qa/rpc-tests/p2p-segwit.py
+++ qa/rpc-tests/p2p-segwit.py
@@ -220,7 +220,7 @@ class SegWitTest(FreicoinTestFramework):
         block_time = self.nodes[0].getblockheader(tip)["mediantime"] + 1
         block = create_block(int(tip, 16), create_coinbase(height), block_time)
         try:
-            finaltx_prevout = self.nodes[0].getblocktemplate({'rules':['segwit','finaltx']})['finaltx']['prevout']
+            finaltx_prevout = self.nodes[0].getblocktemplate({'rules':['segwit','finaltx','auxpow']})['finaltx']['prevout']
         except:
             finaltx_prevout = []
         if finaltx_prevout:
