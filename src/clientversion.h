0ddc0d781dd952b0bbaa36183b788e129e502644
Mark Friedenbach <mark@friedenbach.org>
2021-03-07 14:55:29 -0800

    f '[Version] Switch to Freicoin/Tradecraft versioning scheme for version strings.'

--- src/clientversion.h
+++ src/clientversion.h
@@ -70,7 +70,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
-                         +           CLIENT_VERSION_PATCH;
+                         +       1 * CLIENT_VERSION_PATCH;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
